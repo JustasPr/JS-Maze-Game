99722bf5fcfe7bba3f620a43ec7353d41b38ac2e
Justas Prasukas <prasukas.j@gmail.com>
2021-12-21 19:14:32 +0200

    win animation

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 3;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -124,6 +125,7 @@ horizontals.forEach((row, rowIndex) => {
         unitLength,
         10,
         {
+          label: 'wall',
           isStatic: true
         }
       );
@@ -143,6 +145,7 @@ horizontals.forEach((row, rowIndex) => {
         5,
         unitLength,
         {
+          label: 'wall',
           isStatic: true
         }
       );
@@ -151,19 +154,22 @@ horizontals.forEach((row, rowIndex) => {
 });
 
 const goal = Bodies.rectangle(
-    width - unitLength / 2,
-    height - unitLength / 2,
-    unitLength * .7,
-    unitLength * .7,
-    {
-        isStatic: true
-    }
+  width - unitLength / 2,
+  height - unitLength / 2,
+  unitLength * 0.7,
+  unitLength * 0.7,
+  {
+    label: 'goal',
+    isStatic: true
+  }
 );
 World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: 'ball'
+});
 World.add(world, ball);
 
 document.addEventListener('keydown', event => {
@@ -185,4 +191,24 @@ document.addEventListener('keydown', event => {
   if (event.key === 'a') {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win condition
+
+Events.on(engine, 'collisionStart', event => {
+  event.pairs.forEach(collision => {
+    const labels = ['ball', 'goal'];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y = 1;
+      world.bodies.forEach(body => {
+        if (body.label === 'wall') {
+          Body.setStatic(body, false);
+        }
+      });
+    }
+  });
 });
